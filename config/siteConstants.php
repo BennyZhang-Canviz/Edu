aa34c092cece4660cac2ab6745ef895414172227
Luis Lu (Canviz) <stluislu@gmail.com>
2017-03-31 17:32:00 +0800

    update the comment format

--- config/siteConstants.php
+++ config/siteConstants.php
@@ -34,36 +34,36 @@ class SiteConstants
 }
 class O365ProductLicenses
 {
-    /// <summary>
-    /// Microsoft Classroom Preview
-    /// </summary>
+    /**
+     * Microsoft Classroom Preview
+     */
     const  Classroom   = '80f12768-d8d9-4e93-99a8-fa2464374d34';
-    /// <summary>
-    /// Office 365 Education for faculty
-    /// </summary>
+    /**
+     * Office 365 Education for faculty
+     */
     const  Faculty     = '94763226-9b3c-4e75-a931-5c89701abe66';
-    /// <summary>
-    /// Office 365 Education for students
-    /// </summary>
+    /**
+     * Office 365 Education for students
+     */
     const  Student     = '314c4481-f395-4525-be8b-2ec4bb1e9d91';
-    /// <summary>
-    /// Office 365 Education for faculty
-    /// </summary>
+    /**
+     * Office 365 Education for faculty
+     */
     const  FacultyPro  = '78e66a63-337a-4a9a-8959-41c6654dfb56';
-    /// <summary>
-    /// Office 365 Education for students
-    /// </summary>
+    /**
+     * Office 365 Education for students
+     */
     const StudentPro   = 'e82ae690-a2d5-4d76-8d30-7c6e01e6022e';
 }
 
 class EduConstants
 {
-    /// <summary>
-    /// Office 365 Education for faculty
-    /// </summary>
+    /**
+     * Office 365 Education for faculty
+     */
     const  TeacherObjectType  = 'Teacher';
-    /// <summary>
-    /// Office 365 Education for students
-    /// </summary>
+    /**
+     * Office 365 Education for students
+     */
     const StudentObjectType   = 'Student';
 }
